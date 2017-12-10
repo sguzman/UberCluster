5c2657d9c155eec57125aea02222cfa10a3fdd99
Salvador Guzman <guzmansalv@gmail.com>
2017-12-10 13:16:12 -0800

    added provider for uber login logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
+import { UberLoginProvider } from '../providers/uber-login/uber-login';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { LoginPage } from '../pages/login/login';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UberLoginProvider
   ]
 })
 export class AppModule {}
