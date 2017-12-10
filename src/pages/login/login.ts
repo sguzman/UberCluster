14923798bca635df4273313f12305fdb4ce08a9e
Salvador Guzman <guzmansalv@gmail.com>
2017-12-10 13:05:53 -0800

    form models

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -14,6 +14,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  user: string = '';
+  pass: string = '';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
