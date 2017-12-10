006a4b00781ca1565322d48e666cc3de6932ecfe
Salvador Guzman <guzmansalv@gmail.com>
2017-12-10 15:05:31 -0800

    add http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
 import { UberLoginProvider } from '../providers/uber-login/uber-login';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { UberLoginProvider } from '../providers/uber-login/uber-login';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
