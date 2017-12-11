da75bf0f9a558626b03ffee7cc11af4efa80d332
Salvador Guzman <guzmansalv@gmail.com>
2017-12-10 18:31:46 -0800

    added post for email

--- src/providers/uber-login/uber-login.ts
+++ src/providers/uber-login/uber-login.ts
@@ -2,10 +2,11 @@ import {HttpClient, HttpResponse} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class UberLoginProvider {
-  public login() {
+  public login(): Observable<string> {
     return this.http
       .get('https://auth.uber.com/login/?next_url=https%3A%2F%2Fpartners.uber.com',
         {
@@ -13,6 +14,22 @@ export class UberLoginProvider {
           responseType: 'text'
         })
       .map((response: HttpResponse<string>) => response.headers.get('x-csrf-token'));
+  }
+
+  public email(emailStr: string): Observable<HttpResponse<string>> {
+    return this.login()
+      .flatMap(s => this.http.post('https://auth.uber.com/login/handleanswer', {
+        answer: {
+          type: 'VERIFY_INPUT_USERNAME',
+          userIdentifier: {
+            email: emailStr
+          }
+        },
+        init: true
+      }, {
+        observe: 'response',
+        responseType: 'text'
+      }));
   }
 
   constructor(private http: HttpClient) {
