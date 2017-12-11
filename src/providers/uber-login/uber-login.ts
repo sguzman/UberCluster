13bece4673a15f883d0bcf490ebda43ed08e0bb2
Salvador Guzman <guzmansalv@gmail.com>
2017-12-10 16:15:47 -0800

    get login page

--- src/providers/uber-login/uber-login.ts
+++ src/providers/uber-login/uber-login.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+import {Observable} from "rxjs/Rx";
 
 /*
   Generated class for the UberLoginProvider provider.
@@ -10,7 +13,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class UberLoginProvider {
   public login(user: string) {
-    return this.http.get('https://auth.uber.com/login/?next_url=https%3A%2F%2Fpartners.uber.com')
+    return this.http
+      .get('https://auth.uber.com/login/?next_url=https%3A%2F%2Fpartners.uber.com')
+      .catch(s => Observable.of(s));
   }
 
   constructor(private http: HttpClient) {
