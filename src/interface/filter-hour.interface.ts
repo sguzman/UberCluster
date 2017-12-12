152df9d2040238a1c0c1d2ab4639f98c58713e4a
Salvador Guzman <guzmansalv@gmail.com>
2017-12-12 13:22:52 -0800

    do not use objet for boolean array

--- src/interface/filter-hour.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface FilterHourInterface {
-  hours: boolean[];
-}
--- src/interface/filter-time.interface.ts
+++ src/interface/filter-time.interface.ts
@@ -1,7 +1,6 @@
 import {FilterDayOfWeekInterface} from "./filter-day-of-week.interface";
-import {FilterHourInterface} from "./filter-hour.interface";
 
 export interface FilterTimeInterface {
   dayOfWeek: FilterDayOfWeekInterface;
-  hour: FilterHourInterface;
+  hour: boolean[];
 }
