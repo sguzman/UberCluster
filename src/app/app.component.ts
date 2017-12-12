e4b383de440a6b9856b0e05b399c830300509add
Salvador Guzman <guzmansalv@gmail.com>
2017-12-11 18:13:43 -0800

    set login page as default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {UberLoginPage} from "../pages/uber-login/uber-login";
 
-import {MapPage} from "../pages/map/map";
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = MapPage;
+  rootPage:any = UberLoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
