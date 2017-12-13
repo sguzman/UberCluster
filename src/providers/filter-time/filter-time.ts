96fec83048043c03a02292fd6c5c3367830a4469
Salvador Guzman <guzmansalv@gmail.com>
2017-12-13 11:53:12 -0800

    method for converting days of week to bools

--- src/providers/filter-time/filter-time.ts
+++ src/providers/filter-time/filter-time.ts
@@ -2,8 +2,27 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class FilterTimeProvider {
-  public static filterbyTime(items: any[], filterObject: any): any[] {
-    return items;
+  public static filterbyTime(items: any[], filterObject: FilterTimeInterface): any[] {
+    const daysOfWeek = FilterTimeProvider.boolsFromDayOfWeek(filterObject.dayOfWeek);
+
+    return items.filter(i => {
+      const mnt = moment.unix(i.requestAt);
+      return true;
+    });
+  }
+
+  public static boolsFromDayOfWeek(days: FilterDayOfWeekInterface): boolean[] {
+    const daysOfWeek: boolean[] = [
+      days.sunday,
+      days.monday,
+      days.tuesday,
+      days.wednesday,
+      days.thursday,
+      days.friday,
+      days.saturday,
+    ];
+
+    return daysOfWeek;
   }
 
   constructor() {
